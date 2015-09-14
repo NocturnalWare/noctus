d2fb3d2fa866a9ec5b5ade87d09dfd22a924af05
Jota Vares <joe@eternallynocturnal.com.com>
2015-09-14 11:23:36 -0400

    Tests Login Controller
    Adds User Seeder & tests for login

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $accounts = array(
+                [
+                	'name' => 'Jason Putnam',
+                    'email'       => 'owner@eternallynocturnal.com',
+                    'password'    => \Hash::make('Password123'),
+                ],	                
+                [
+                	'name' => 'Joe Tavares',
+                    'email'       => 'joe@eternallynocturnal.com',
+                    'password'    => \Hash::make('Password123'),
+                ],	                
+                [
+                	'name' => 'Halie Butki',
+                    'email'       => 'halie@eternallynocturnal.com',
+                    'password'    => \Hash::make('Password123'),
+                ],
+        );
+
+        foreach ($accounts as $account) {
+            $user = \App\User::create($account);
+    	}
+	}
+}
