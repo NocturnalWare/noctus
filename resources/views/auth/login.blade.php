60e1b7f8d0cfaf3620f7bc12d722461f8c226466
Jota Vares <joe@eternallynocturnal.com.com>
2015-09-15 19:58:49 -0400

    Adds master & login styling

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,23 +1,36 @@
-<!-- resources/views/auth/login.blade.php -->
+@extends('layouts.master')
+<div class="container">
+    <div class="content">
+        <img src="https://www.eternallynocturnal.com/images/witeskull.png" class="img-responsive" />
+        <form method="POST" action="/auth/login">
+            {!! csrf_field() !!}
 
-<form method="POST" action="login">
-    {!! csrf_field() !!}
+            <div class="col-sm-12">
+                <div class="col-sm-12">
+                    <label class="en-label">Email</label>
+                </div>
+                <div class="col-sm-12">
+                    <input type="email" name="email" value="{{ old('email') }}">
+                </div>
+            </div>
 
-    <div>
-        Email
-        <input type="email" name="email" value="{{ old('email') }}">
+            <div class="col-sm-12">
+                <div class="col-sm-12">
+                    <label class="en-label">Password</label>
+                </div>
+                <div class="col-sm-12">
+                    <input type="password" name="password" id="password">
+                </div>
+            </div>
+
+            <div class="col-sm-12">
+                <input type="checkbox" name="remember"> 
+                <span style="color:#fff">Remember Me</span>
+            </div>
+
+            <div style="margin-top:20px;">
+                <button class="btn btn-sm btn-primary" type="submit">Login</button>
+            </div>
+        </form>
     </div>
-
-    <div>
-        Password
-        <input type="password" name="password" id="password">
-    </div>
-
-    <div>
-        <input type="checkbox" name="remember"> Remember Me
-    </div>
-
-    <div>
-        <button type="submit">Login</button>
-    </div>
-</form>
+</div>
