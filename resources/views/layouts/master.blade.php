396db46b48d45e8d464b37c131641ea6142b5431
Jota Vares <joe@eternallynocturnal.com.com>
2015-09-15 11:25:55 -0400

    Adds resources
    jquery, vue, bootstrap, fontawesome

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,6 @@
+<link rel="stylesheet" href="../resources/assets/font-awesome/css/font-awesome.min.css">
+
+<link rel="stylesheet" href="../resources/assets/bootstrap/css/bootstrap-theme.min.css">
+<script src="../resources/assets/js/jquery.min.js"></script>
+<script src="../resources/assets/js/vue.min.js"></script>
+<script src="../resources/assets/bootstrap/js/bootstrap.min.js"></script>
