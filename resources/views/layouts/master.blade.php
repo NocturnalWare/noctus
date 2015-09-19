44bb5d80f44347575c70bab3f8826f8bc5f40fd7
Jota Vares <joe@eternallynocturnal.com.com>
2015-09-19 18:52:38 -0400

    Again?

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,10 +2,10 @@
   	<meta charset="UTF-8">
   	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta property="og:image" content="https://www.eternallynocturnal.com/images/blackskull.jpg">
-    <link rel="stylesheet" href="public/font-awesome/css/font-awesome.min.css">
-    <link rel="stylesheet" href="public/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="public/bootstrap/css/bootstrap-theme.min.css">
-    <script src="public/js/bundle.js"></script>
+    <link rel="stylesheet" href="/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/bootstrap/css/bootstrap-theme.min.css">
+    <script src="/js/bundle.js"></script>
         <title>Noctus</title>
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
