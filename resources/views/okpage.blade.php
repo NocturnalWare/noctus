ee87055afd8c338cfdff31d571bb48bbd6a0d3b4
Jota Vares <joe@eternallynocturnal.com.com>
2015-09-19 23:24:49 -0400

    Changes style of main page.

--- resources/views/okpage.blade.php
+++ resources/views/okpage.blade.php
@@ -1,17 +1,29 @@
 @extends('layouts.backend')
 @section('content')
 
-	<div class="col-lg-4 col-sm-6 well well-sm">
-		Product Manager<br>
-		<i class="fa fa-gift" style="font-size:100px;"></i>
+	<div class="col-lg-4 col-sm-6">
+		<div class="col-sm-4"></div>
+		<div class="col-sm-4">
+			Product Manager<br>
+			<i class="fa fa-gift" style="font-size:100px;"></i>
+		</div>
+		<div class="col-sm-4"></div>
 	</div>
 	<div class="col-lg-4 col-sm-6 well well-sm">
-		Contact Manager<br>
-		<i class="fa fa-users" style="font-size:100px;"></i>
+		<div class="col-sm-4"></div>
+		<div class="col-sm-4">
+			Contact Manager<br>
+			<i class="fa fa-users" style="font-size:100px;"></i>
+		</div>
+		<div class="col-sm-4"></div>
 	</div>
 	<div class="col-lg-4 col-sm-6 well well-sm">
-		Venue Manager<br>
-		<i class="fa fa-university" style="font-size:100px;"></i>
+		<div class="col-sm-4"></div>
+		<div class="col-sm-4">
+			Venue Manager<br>
+			<i class="fa fa-university" style="font-size:100px;"></i>
+		</div>
+		<div class="col-sm-4"></div>
 	</div>
 </div>
 <div class="col-sm-12 en-text">
