a81e64aac1621613c36020b9258a3354ea63748e
Jota Vares <joe@eternallynocturnal.com.com>
2015-09-19 20:40:57 -0400

    Again.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,5 +1,6 @@
 <head>
   	<meta charset="UTF-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta property="og:image" content="https://www.eternallynocturnal.com/images/blackskull.jpg">
     <link rel="stylesheet" href="/font-awesome/css/font-awesome.min.css">
     <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
@@ -60,7 +61,6 @@
         </style>
             
     </head>
-    <body>
 
 
 
@@ -68,10 +68,11 @@
 
 @include('layouts.topcarousel')
 @include('layouts.publicnav')
-@yield('content')
+<body>
+    @yield('content')
+</body>
 
 </div>
 
-</body>
 </html>
 
