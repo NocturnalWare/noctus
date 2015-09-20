a424efe7fe5c7bc122a56397e50da2fccbffaa21
Jota Vares <joe@eternallynocturnal.com.com>
2015-09-19 22:15:58 -0400

    Fixes name schema on migrations for primary tables

--- database/migrations/2015_09_20_021454_create_inventory_table.php
+++ database/migrations/2015_09_20_021454_create_inventory_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Prices extends Migration
+class CreateInventoryTable extends Migration
 {
     /**
      * Run the migrations.
--- database/migrations/2015_09_20_021508_create_promotions_table.php
+++ database/migrations/2015_09_20_021508_create_promotions_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Promotions extends Migration
+class CreatePromotionsTable extends Migration
 {
     /**
      * Run the migrations.
