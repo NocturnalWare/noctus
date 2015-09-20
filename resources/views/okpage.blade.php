09c0a2dd74b6dbf8b178a02906149a843efb7843
Jota Vares <joe@eternallynocturnal.com.com>
2015-09-19 23:32:53 -0400

    Adds missed curly braces.

--- resources/views/okpage.blade.php
+++ resources/views/okpage.blade.php
@@ -3,7 +3,7 @@
 
 	<div class="col-lg-4 col-sm-6">
 		<div class="col-sm-4"></div>
-		<div class="col-sm-4 well well-sm en-big-well-button" href="route('productManager')">
+		<div class="col-sm-4 well well-sm en-big-well-button" href="{{route('productManager')}}">
 			Product Manager<br>
 			<i class="fa fa-gift" style="font-size:100px;"></i>
 		</div>
