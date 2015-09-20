659da9186afebaff28b024e6fcf64e2e29c9056f
Jota Vares <joe@eternallynocturnal.com.com>
2015-09-19 23:29:54 -0400

    Updates CSS on big well buttons

--- resources/views/okpage.blade.php
+++ resources/views/okpage.blade.php
@@ -3,7 +3,7 @@
 
 	<div class="col-lg-4 col-sm-6">
 		<div class="col-sm-4"></div>
-		<div class="col-sm-4 well well-sm">
+		<div class="col-sm-4 well well-sm en-big-well-button">
 			Product Manager<br>
 			<i class="fa fa-gift" style="font-size:100px;"></i>
 		</div>
@@ -11,7 +11,7 @@
 	</div>
 	<div class="col-lg-4 col-sm-6">
 		<div class="col-sm-4"></div>
-		<div class="col-sm-4 well well-sm">
+		<div class="col-sm-4 well well-sm en-big-well-button">
 			Contact Manager<br>
 			<i class="fa fa-users" style="font-size:100px;"></i>
 		</div>
@@ -19,13 +19,13 @@
 	</div>
 	<div class="col-lg-4 col-sm-6">
 		<div class="col-sm-4"></div>
-		<div class="col-sm-4 well well-sm">
+		<div class="col-sm-4 well well-sm en-big-well-button">
 			Venue Manager<br>
 			<i class="fa fa-university" style="font-size:100px;"></i>
 		</div>
 		<div class="col-sm-4"></div>
 	</div>
-</div>
+
 <div class="col-sm-12 en-text">
 <h1>Welcome to Noctus! This is the database engine that will run our primary services!</h1>
 
