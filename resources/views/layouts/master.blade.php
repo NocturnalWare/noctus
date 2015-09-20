b3cec3f5f2516f9c966c0f728b049d52591e3830
Jota Vares <joe@eternallynocturnal.com.com>
2015-09-19 20:39:22 -0400

    Try Again.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,6 +1,5 @@
 <head>
   	<meta charset="UTF-8">
-  	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta property="og:image" content="https://www.eternallynocturnal.com/images/blackskull.jpg">
     <link rel="stylesheet" href="/font-awesome/css/font-awesome.min.css">
     <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
