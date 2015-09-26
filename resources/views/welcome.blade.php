2623044766c17a32bcb5d1b8d9dc28fec70ea7c0
Jota Vares <joe@eternallynocturnal.com.com>
2015-09-26 10:36:34 -0400

    Adds content example
    Adds featured items Area.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,25 +1,62 @@
 @extends('layouts.master')
 @section('content')
 <div class="col-sm-12" style="margin-left:5%;width:90%;text-align:center;opacity:1.0;color:#fff;background-color:#000;padding:15px;border-radius:4px;">
-	<div class="col-md-6 col-sm-12 en-content-box">
-		<h3>Nightfall</h3>
-			<p style="font-size:1.2em">Last year immediately after the show, we launched our website with lofty goals in mind <br>(it looked like crap.)<br><br>
-				A few weeks later we moved to our "Dusk" theme because we had to get ready for nightfall <br>(it looked a bit less like crap.)<br><br>
+	<div id="contentBlog" class="col-md-6 col-sm-12">
+		<div class="col-md-12 col-sm-12 en-content-box">
+			<h3 class="en-blog-title">Nightfall</h3>
+				<p style="font-size:1.2em">
+					Last year immediately after the show, we launched our website with lofty goals in mind 
+					
+					<br><br>it looked like crap.<br><br>
 
-				 We hope you like the new "Nightfall" look -- we've got so much more than a cool website theme and new clothes this time around.
+					A few weeks later we moved to our "Dusk" theme because we had to prepare for nightfall.
 
-				<br><br>
+					<br><br>it looked a bit less like crap.<br><br>
 
-				Thanks for sticking around,
-				<br><br>
-				<img height="10%" class="img-responsive" src="https://www.eternallynocturnal.com/images/mainenheader.png" />
-			</p>
+					 We hope you like the new "Nightfall" look -- we've got so much more than a cool website theme and new clothes this time around.
+
+					<br><br>
+
+					Thanks for sticking around,
+					<br><br>
+					<center><img height="10%" class="img-responsive" src="https://www.eternallynocturnal.com/images/mainenheader.png" /></center>
+				</p>
+		</div>
+		<div class="col-md-12 col-sm-12 en-content-box">
+			<h3 class="en-blog-title">Take 1. Scene: Northville Woods. Acti...cops.</h3>
+				<p>
+					We tried to shoot some footage in the woods near the Northville asylum (not the buildings) 
+					because there's very few people back there to interrupt us while we do makeup and set up 
+					scenes. See also: pour biodegradeable fake blood everywhere.<br><br>
+					Two of us went ahead to find a suitable tree to climb and tie a pulley system to for a later scene -- mission accomplished --
+					and by the time we got back to the group there was already a police officer kindly asking us to leave.
+					We obliged. They took our names, you know, just in case we try to come back to film later. We ended up planning a different 
+					scene, and moving approximately 1/2 mile down the road. Much ruckus was caused in a van in the middle of the night in Northville.
+					<br><br>
+					The cops had changed shifts at that point -- SO IT WASN'T THE SAME OFFICER WHO ASKED US TO VACATE THE SECOND LOCATION.
+					<br><br>
+					<br>
+					<span style="font-size:1.3em">There's a valuable lesson here: 
+					<br>
+					Since we never get bothered when we're filming in the hood, police officers in Northville must be bored.
+					<br>
+					Also, they're paid ~20k more per year than their actual-crime-fighting counterparts in Detroit city proper. 
+					<br><br>
+					That's interesting. </span>
+					<br><br>
+					Don't forget to vote to end this "small government" bullshit parade in 2016.
+
+				</p>
+		</div>
 	</div>
 	<div class="col-md-2"></div>
-	<div class="col-md-3 col-sm-12 en-content-box" style="max-height:200px;">
+	<div class="col-md-3 col-sm-12 en-content-box">
 		<h3>Upcomming Shows:</h3>
 		BATTLE OF THE BANDS Saturday 9/26<br>
 		PUTNAMS HALLOWEEN Friday 10/16<br>
+	</div>
+	<div class="col-md-6 col-sm-12 en-content-box" >
+		<h3>Featured items:<br>
 	</div>
 </div>
 @stop
