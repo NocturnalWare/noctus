4b874322aba45a64e9e9d6aa34e9d5d0e1b6a593
Jota Vares <joe@eternallynocturnal.com.com>
2015-10-01 03:44:54 -0400

    Changes the product index view slightly.

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,13 +1,15 @@
 @extends('layouts.master')
 
 @section('content')
-<div class="col-sm-12" style="background-color:#000;color:#fff">
+<div class="col-sm-12"style="background-color:#000;color:#fff;padding:20px;">
 	@foreach($products as $pro)
 		<center>
-			<div class=" col-sm-6 col-md-4 well well-sm" >
-				<label class="en-label-bl">{{$pro->name}} {{$pro->price}}</label><br>
-				<div style="background-color:#000;">
-					<img class="img-responsive" style="max-height:600px;margin-left:5%" src="https://www.eternallynocturnal.com/store/public/images/products/{{$pro->main_image}}">
+			<div class=" col-sm-6 col-md-4" style="border:20px solid #000;border-radius:10px;">
+				<div style="width:90%">
+					<label class="en-label-bl" style="background-color:#aaa;width:100%;" >{{$pro->name}} {{$pro->price}}</label><br>
+					<div style="background-color:#000;">
+						<img class="img-responsive" style="max-height:600px;" src="https://www.eternallynocturnal.com/store/public/images/products/{{$pro->main_image}}">
+					</div>	
 				</div>	
 			</div>
 		</center>
