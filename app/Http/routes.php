f2b3216c8e81753b21c0b3bcbd776977cb8a7c9b
Jota Vares <joe@eternallynocturnal.com.com>
2015-10-01 02:59:22 -0400

    Adds meat to product model.
    Adds route for products.
    Adds product index.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -28,6 +28,7 @@ Route::get('auth/logout', 'Auth\AuthController@getLogout');
 Route::get('auth/register', 'Auth\AuthController@getRegister');
 Route::post('auth/register', 'Auth\AuthController@postRegister');
 
+Route::resource('products', 'ProductsController');
 
 
 Route::get('productmanager/index', array('as' => 'productManager', 'uses' => 'ProductManagerController@index'));
