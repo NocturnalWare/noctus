d81b5586d8b1025607c0915bb9756047b446c361
Jota Vares <joe@eternallynocturnal.com.com>
2015-11-29 18:59:26 -0500

    Fixes carousel icons.

--- resources/views/bands/video-carousel.blade.php
+++ resources/views/bands/video-carousel.blade.php
@@ -25,11 +25,11 @@
 
   <!-- Controls -->
   <a class="left carousel-control" href="#vide-carousel" role="button" data-slide="prev">
-    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <i class="fa fa-chevron-left"></i>
     <span class="sr-only">Previous</span>
   </a>
   <a class="right carousel-control" href="#vide-carousel" role="button" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <i class="fa fa-chevron-right"></i>
     <span class="sr-only">Next</span>
   </a>
 </div>
