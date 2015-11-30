5d426a5dad3489c8755f0ec1b4dd338bfd596170
Jota Vares <joe@eternallynocturnal.com.com>
2015-11-30 00:06:31 -0500

    Adds inventory model.
    Adds method to check stock, will not show products that have 0.

--- app/Inventory.php
+++ app/Inventory.php
@@ -6,5 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class Inventory extends Model
 {
-    //
+    protected $rules = [];
+    protected $fillable = [
+    	'product_id',
+    	'xsmall',
+    	'small',
+    	'medium',
+    	'large',
+    	'xlarge',
+    	'xxlarge',
+    	'xxxlarge',
+    	'onesize'
+    ];
+
+    protected $table = 'inventories';
+
+    public function inventories(){
+    	return $this->belongsTo('\App\Product', 'id', 'product_id');
+    }
+
+    public function checkStock(){
+    	return $this->xsmall + $this->small + $this->medium + $this->large + $this->xlarge + $this->xxlarge + $this->xxxlarge + $this->onesize;
+    }
 }
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -3,18 +3,20 @@
 @section('content')
 <div class="col-sm-12"style="background-color:#000;color:#fff;padding:20px;">
 	@foreach($products as $pro)
-		<center>
-			<a href="{{route('products.show', $pro->id)}}">
-				<div class=" col-sm-6 col-md-4" style="border:20px solid #000;border-radius:10px;">
-					<div style="width:90%">
-						<div style="background-color:#000;">
-							<img class="img-responsive" style="max-height:600px;" src="https://www.eternallynocturnal.com/store/public/images/products/{{$pro->main_image}}">
+		@if($pro->inventories->checkStock() !== 0)
+			<center>
+				<a href="{{route('products.show', $pro->id)}}">
+					<div class=" col-sm-6 col-md-4" style="border:20px solid #000;border-radius:10px;">
+						<div style="width:90%">
+							<div style="background-color:#000;">
+								<img class="img-responsive" style="max-height:600px;" src="https://www.eternallynocturnal.com/store/public/images/products/{{$pro->main_image}}">
+							</div>	
+							<label class="en-label-bl" style="background-color:#aaa;width:100%;" > {{$pro->inventories->checkStock()}} {{$pro->name}} ${{$pro->price}}</label><br>
 						</div>	
-						<label class="en-label-bl" style="background-color:#aaa;width:100%;" >{{$pro->name}} ${{$pro->price}}</label><br>
-					</div>	
-				</div>
-			</a>
-		</center>
+					</div>
+				</a>
+			</center>
+		@endif
 	@endforeach
 </div>
 @stop
