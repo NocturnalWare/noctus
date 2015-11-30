2ceb9f7e6843c3e5b2436f88ad2906699ef4fc6b
Jota Vares <joe@eternallynocturnal.com.com>
2015-11-29 19:42:56 -0500

    Adds shows, moves carousels around
    touches up some CSS bits that were bothering me.

---
+++ resources/views/landing/shows-carousel.blade.php
@@ -0,0 +1,38 @@
+<div id="products-carousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#products-carousel" data-slide-to="0" class="active"></li>
+    <li data-target="#products-carousel" data-slide-to="1"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <center>
+        <div class="col-sm-12">
+          Toys For Tots
+          <a href="https://www.facebook.com/events/1700093976886189/" target="_blank"><img class="img-responsive" style="100%;max-height:500px;" src="https://www.eternallynocturnal.com/images/toysfortots.jpg"></a>
+        </div>
+      </center>
+    </div>
+
+    <div class="item">
+      A Not So Silent Night
+      <div class="col-sm-12">
+        <center>
+          <a href="https://www.facebook.com/events/1509324312698895/" target="_blank"><img class="img-responsive" style="100%;max-height:500px;" src="https://www.eternallynocturnal.com/images/silentnight.jpg"></a>
+        </center>
+      </div>
+    </div>
+  </div>
+
+  <!-- Controls -->
+  <a class="left carousel-control" href="#products-carousel" role="button" data-slide="prev">
+    <i class="fa fa-chevron-left"></i>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#products-carousel" role="button" data-slide="next">
+    <i class="fa fa-chevron-right"></i>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,34 +1,37 @@
 @extends('layouts.master')
 @section('content')
 <div class="window1">
-		<div class="col-md-8 col-sm-12 en-content-box">
-			<h3 class="en-blog-title" style="text-align:center">Site Update</h3>
-				<p style="font-size:1.2em;text-align:center">
-					Last year immediately after Putnam's Halloween, we launched our website with lofty goals in mind 
-					
-					<br><br>it looked like crap.<br><br>
+		<div class="row">
+			<div class="col-md-8 col-sm-12 en-content-box">
+				<h3 class="en-blog-title" style="text-align:center">Site Update</h3>
+					<p style="font-size:1.2em;text-align:center">
+						Last year immediately after Putnam's Halloween, we launched our website with lofty goals in mind 
+						
+						<br><br>it looked like crap.<br><br>
 
-					A few weeks later we moved to our "Dusk" theme because we had to prepare for nightfall.
+						A few weeks later we moved to our "Dusk" theme because we had to prepare for nightfall.
 
-					<br><br>it looked a bit less like crap.<br><br>
+						<br><br>it looked a bit less like crap.<br><br>
 
-					 We hope you like the new look -- we're going to be bringing even more art and events along with our new clothing line.
+						 We hope you like the new look -- we're going to be bringing even more art and events along with our new clothing line.
 
-					<br><br>
+						<br><br>
 
-					Thanks for sticking around,
-					<br><br>
-						<center>
-							<img height="10%" class="img-responsive" src="https://www.eternallynocturnal.com/images/mainenheader.png" />
-						</center>
-				</p>
-		</div>
-		<div class="hidden-sm col-md-1" style="width:40px">&nbsp</div>
-		<div class="col-md-3 col-sm-12 en-content-box" style="text-align:center;height:100%;max-height:400px;">
-			<h3 class="en-blog-title" style="text-align:center">Upcoming Shows:</h3>
-			PUTNAMS HALLOWEEN Friday 10/16<br>
+						Thanks for sticking around,
+						<br><br>
+							<center>
+								<img height="10%" class="img-responsive" src="https://www.eternallynocturnal.com/images/mainenheader.png" />
+							</center>
+					</p>
+			</div>
+			<div class="hidden-sm col-md-1" style="width:40px">&nbsp</div>
+			<div class="col-md-3 col-sm-12 en-content-box" style="text-align:center;">
+				<h3 class="en-blog-title" style="text-align:center">Upcoming Shows</h3>
+				@include('landing.shows-carousel')
+			</div>
 		</div>
 		
+		<div class="row">
 		<div class="col-md-6 col-sm-12 en-content-box embed-responsive-16by9"style="opacity:1;text-align:center;overflow:visible;z-index:5;position:relative;">
 			@include('landing.video-carousel')
 		</div>
@@ -45,6 +48,7 @@
 			</div>
 		</div>
 	</div>
+		</div>
 </div>
 
 @stop
