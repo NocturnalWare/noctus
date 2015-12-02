9a4cdc500c4fad9d8e8888202e5d69e1cce301f6
Jota Vares <joe@eternallynocturnal.com.com>
2015-12-02 02:06:22 -0500

    fixes Logout

--- resources/views/layouts/adminnav.blade.php
+++ resources/views/layouts/adminnav.blade.php
@@ -39,7 +39,7 @@
           </ul>
         </li>
         <li>
-          <a style="background-color:#000;color:#fff" type="button" class="dropdown nav-button-etnoc btn btn-lg" href="../../../noctus/public/auth/logout">
+          <a style="background-color:#000;color:#fff" type="button" class="dropdown nav-button-etnoc btn btn-lg" href="{{route('logout')}}">
               <b>Logout</b>
           </a>
         </li>
