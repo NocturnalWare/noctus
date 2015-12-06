4ab15fa8af0908ffbe52b390cbe363bb9141ce6c
Jota Vares <joe@eternallynocturnal.com.com>
2015-12-06 10:57:50 -0500

    Changes favicon and header text

--- resources/views/layouts/adminnav.blade.php
+++ resources/views/layouts/adminnav.blade.php
@@ -3,7 +3,7 @@
   <div class="container-fluid"  style="background-color:#000;">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
-      <a class="navbar-brand" href="http://www.eternallynocturnal.com" style=""><img height="130%" src="http://54.149.181.47/images/mainenheader.png" /></a>
+      <a class="navbar-brand" href="http://www.eternallynocturnal.com" style=""><img height="190%" src="https://www.eternallynocturnal.com/images/WEBSITEBANNERTEXT.png" /></a>
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
         <span class="sr-only">Toggle navigation</span>
         <span style="padding:0px"class="icon-bar"></span>
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,7 +2,7 @@
   	<meta charset="UTF-8">
   	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	  <meta property="og:image" content="http://54.149.181.47/images/blackskull.jpg">
-    <link rel="shortcut icon" href="http://159.203.77.45/favicon.ico" />
+    <link rel="shortcut icon" href="https://www.eternallynocturnal.com/favicon.ico" />
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet">
     <link rel="stylesheet" href="http://159.203.77.45/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="http://159.203.77.45/bootstrap/css/bootstrap-theme.min.css">
