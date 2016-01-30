8e22db653134e2a845ef45db97da79462144033e
Jota Vares <joe@eternallynocturnal.com.com>
2016-01-30 14:26:52 -0500

    updates the shows carousel.

--- resources/views/landing/shows-carousel.blade.php
+++ resources/views/landing/shows-carousel.blade.php
@@ -5,6 +5,7 @@
     <li data-target="#shows-carousel" data-slide-to="1"></li>
     <li data-target="#shows-carousel" data-slide-to="2"></li>
     <li data-target="#shows-carousel" data-slide-to="3"></li>
+    <li data-target="#shows-carousel" data-slide-to="4"></li>
   </ol>
 
   <!-- Wrapper for slides -->
@@ -33,6 +34,15 @@
       <div class="col-sm-12">
         <center>
           <a href="https://www.facebook.com/events/1615891782005933/" target="_blank"><img class="img-responsive" style="100%;max-height:500px;padding-bottom:35px;" src="https://www.eternallynocturnal.com/images/shows/valentines.jpg"></a>
+        </center>
+      </div>
+    </div>
+
+    <div class="item">
+      Athena's Grace "Martyrs" EP Release
+      <div class="col-sm-12">
+        <center>
+          <a href="https://www.facebook.com/events/499669176872150/" target="_blank"><img class="img-responsive" style="100%;max-height:500px;padding-bottom:35px;" src="https://www.eternallynocturnal.com/images/shows/agshow.jpg"></a>
         </center>
       </div>
     </div>
