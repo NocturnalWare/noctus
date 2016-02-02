20fc348a88cbc76ed993e6d5f215509c01296d9a
Jota Vares <joe@eternallynocturnal.com.com>
2016-02-02 09:17:22 -0500

    Adds table.

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -3,16 +3,20 @@
 
 <div style="color:#fff;margin-top:80px;" class="col-sm-6">
 <?php $tickettotal = 0;?>
-	@foreach($ticketsales as $sale)
-	<div class="col-sm-12">
-		<div class="col-sm-6">
-			{{$sale->getShippingAddress()->ship_f_name}} {{$sale->getShippingAddress()->ship_l_name}}
-		</div>
-		<div class="col-sm-2">
-			@foreach($sale->cartsBySale as $lol){{$lol->quantity}} <?php $tickettotal += $lol->quantity?> @endforeach<br>
-		</div>
-	</div>
-	@endforeach
+		<table class="table"> <tr>
+			<th>Name</th>
+			<th>Tickets</th>
+			@foreach($ticketsales as $sale)
+				<tr class="table-striped">
+					<td>
+						{{$sale->getShippingAddress()->ship_f_name}} {{$sale->getShippingAddress()->ship_l_name}}
+					</td>
+					<td>
+						@foreach($sale->cartsBySale as $lol){{$lol->quantity}} <?php $tickettotal += $lol->quantity?> @endforeach<br>
+					</td>
+				</tr>
+			@endforeach
+	</table>
 </div>
 <div style="color:#fff;margin-top:80px;" class="col-sm-6">
 	<h3>{{$tickettotal}}</h3>
