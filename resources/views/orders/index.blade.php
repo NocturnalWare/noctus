b3901e8cbc4154cc4825c537050ac168bce69881
Jota Vares <joe@eternallynocturnal.com.com>
2016-02-02 09:11:25 -0500

    Adds total.

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -1,13 +1,15 @@
 @extends('layouts.master')
 @section('content')
 
-<div style="color:#fff;margin-top:80px;">
-
+<div style="color:#fff;margin-top:80px;" class="col-sm-6">
+<?php $tickettotal = 0;?>
 	@foreach($ticketsales as $sale)
-		{{$sale->getShippingAddress()->ship_f_name}} {{$sale->getShippingAddress()->ship_l_name}} || @foreach($sale->cartsBySale as $lol){{$lol->quantity}}@endforeach<br>
+		{{$sale->getShippingAddress()->ship_f_name}} {{$sale->getShippingAddress()->ship_l_name}} || @foreach($sale->cartsBySale as $lol){{$lol->quantity}} {{$tickettotal += $lol->quantity}}@endforeach<br>
 	@endforeach
 </div>
-
+<div style="color:#fff;margin-top:80px;" class="col-sm-6">
+	<h3>{{$tickettotal}}</h3>
+</div>
 
 <div id="orderManager">
 
