9f5522e4e2740ae500324d3991b5e7b779e603e8
Jota Vares <joe@eternallynocturnal.com.com>
2016-02-02 09:14:11 -0500

    hides total on willcall.

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -4,7 +4,14 @@
 <div style="color:#fff;margin-top:80px;" class="col-sm-6">
 <?php $tickettotal = 0;?>
 	@foreach($ticketsales as $sale)
-		{{$sale->getShippingAddress()->ship_f_name}} {{$sale->getShippingAddress()->ship_l_name}} || @foreach($sale->cartsBySale as $lol){{$lol->quantity}} {{$tickettotal += $lol->quantity}}@endforeach<br>
+	<div class="col-sm-12">
+		<div class="col-sm-6">
+			{{$sale->getShippingAddress()->ship_f_name}} {{$sale->getShippingAddress()->ship_l_name}}
+		</div>
+		<div class="col-sm-2">
+			@foreach($sale->cartsBySale as $lol){{$lol->quantity}} <?php $tickettotal += $lol->quantity?> @endforeach<br>
+		</div>
+	</div>
 	@endforeach
 </div>
 <div style="color:#fff;margin-top:80px;" class="col-sm-6">
