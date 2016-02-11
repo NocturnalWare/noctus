cca1f78f65920084883ec749496569b6de4069cc
Jota Vares <joe@eternallynocturnal.com.com>
2016-02-10 22:01:26 -0500

    updates orders index to remove projectest

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -1,10 +1,6 @@
 
 
 <div style="color:#fff;margin-top:80px;" class="col-sm-6">
-
-	<script src="https://demo.makeros.com/js/projectwidget.js" type="text/javascript"></script>
-                    <div id="create-a-makeros-project" provider-name="demo"></div>    
-                
 <?php $tickettotal = 0;?>
 		<table class="table"> <tr>
 			<th>Name</th>
