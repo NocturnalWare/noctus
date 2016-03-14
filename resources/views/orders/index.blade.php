1883831a67712ff2b7dfe43eeedb3a7d84d3c437
Jota Vares <joe@eternallynocturnal.com.com>
2016-03-14 17:42:47 -0400

    updates orders view

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -23,7 +23,7 @@
 						<td>
 							{{$lol->quantity}} <?php $tickettotal += $lol->quantity?> 
 						</td>
-							@endforeach<br>
+							@endforeach
 					</tr>
 				@endif
 			@endforeach
@@ -33,8 +33,6 @@
 				</td>
 			</tr>
 	</table>
-</div>
-<div style="color:#fff;margin-top:80px;" class="col-sm-6">
 </div>
 
 <div id="orderManager">
