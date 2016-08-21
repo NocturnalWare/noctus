248f440fcaa05281848f70ff1f64907cc86cc898
Jota Vares <joe@eternallynocturnal.com.com>
2016-08-21 12:32:30 -0400

    Updates show carousel

--- resources/views/landing/shows-carousel.blade.php
+++ resources/views/landing/shows-carousel.blade.php
@@ -2,17 +2,29 @@
   <!-- Indicators -->
   <ol class="carousel-indicators" style="margin-top:202px;">
     <li data-target="#shows-carousel" data-slide-to="0" class="active"></li>
+    <li data-target="#shows-carousel" data-slide-to="1"></li>
   </ol>
 
   <!-- Wrapper for slides -->
   <div class="carousel-inner" role="listbox">
 
     <div class="item active">
-      As We Divide Album Release
+      Shapes & Colors/Stories Untold
       <div class="col-sm-12">
         <center>
-          <a href="https://www.facebook.com/events/2014973802061171/" target="_blank">
-            <img class="img-responsive" style="100%;max-height:500px;padding-bottom:35px;" src="./images/aswerelease.jpg">
+          <a href="https://www.facebook.com/events/1443115302380538/" target="_blank">
+            <img class="img-responsive" style="100%;max-height:500px;padding-bottom:35px;" src="https://s3-us-west-2.amazonaws.com/etnoc/shows/Shapes9-10-2016.jpg">
+          </a>
+        </center>
+      </div>
+    </div>
+
+    <div class="item">
+      The Icarus Account & Daisyhead
+      <div class="col-sm-12">
+        <center>
+          <a href="https://www.facebook.com/events/174210819651125/" target="_blank">
+            <img class="img-responsive" style="100%;max-height:500px;padding-bottom:35px;" src="https://s3-us-west-2.amazonaws.com/etnoc/shows/Daisyhead9-11-2016.jpg">
           </a>
         </center>
       </div>
