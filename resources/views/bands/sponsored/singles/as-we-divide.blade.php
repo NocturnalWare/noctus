0d8288672a59e8d068ca2059b8e4405f6eba345d
Jota Vares <joe@manulith.com>
2016-08-24 11:50:16 -0400

    adds credit to undead studios

--- resources/views/bands/sponsored/singles/as-we-divide.blade.php
+++ resources/views/bands/sponsored/singles/as-we-divide.blade.php
@@ -2,25 +2,25 @@
 
 @section('facebook-tags')
           <meta property="og:image" content="https://s3-us-west-2.amazonaws.com/etnoc/images/AWD_fb.jpg" />
-          <meta property="og:description" content="To show you all how much we appreciate you, enjoy a 
+          <meta property="og:description" content="To show you all how much we appreciate you, enjoy a
 			free download of our new single “Ease The Pain”" />
           <meta property="og:title" content="As We Divide - Ease The Pain" />
 @stop
 
-@section('content') 
+@section('content')
 	<div class="col-xs-12 col-md-8 col-md-offset-2" style="padding-top:20px">
 		<center>
 
 			<h3>
 				<div class="col-xs-12">&nbsp</div>
 				<br>
-				The amount of support that you have all been giving 
+				The amount of support that you have all been giving
 				As We Divide and Eternally Nocturnal has been amazing!
 				<br>
 				<br>
 				To show you all how much
-				 we appreciate it, below is a 
-				free download of our new single “Ease The Pain” 
+				 we appreciate it, below is a
+				free download of our new single “Ease The Pain”
 				<br>
 				<br>
 				Please <span class="fb-share-button" data-href="https://eternallynocturnal.com/singles/as-we-divide" data-layout="button"></span>
@@ -33,8 +33,8 @@
 					<source src="https://s3-us-west-2.amazonaws.com/etnoc/songs/As_We_Divide-Ease_The_Pain.mp3" type="audio/mp3">
 				</audio>
 			</div>
-			
-					
+
+
 				<div class="col-xs-12">&nbsp</div>
 				<br>
 				<a download href="http://tinyurl.com/z2kc6vz">
@@ -45,15 +45,25 @@
 				<br>
 				<br>
 				<br>
-			
+
 			<a style="color:#fff;font-size:1.5em" class="btn btn-sm mobile-nav-button-etnoc" href="https://www.facebook.com/AsWeDivide">
-				As We Divide <i class="fa fa-facebook-official"></i> 
+				As We Divide <i class="fa fa-facebook-official"></i>
 			</a>
 
-			
+
 			<a style="color:#fff;font-size:1.5em" class="btn btn-sm mobile-nav-button-etnoc" href="https://www.facebook.com/EternallyNocturnal">
-				Eternally Nocturnal <i class="fa fa-facebook-official"></i> 
+				Eternally Nocturnal <i class="fa fa-facebook-official"></i>
 			</a>
+
+			<div class="col-xs-12">
+				Recorded and produced at <a href="http://www.undeadstudiosmi.com/">Undead Studios</a>
+				<a style="color:#fff;font-size:1.5em" class="btn btn-sm mobile-nav-button-etnoc" href="https://www.facebook.com/undeadstudiosmi">
+					<i class="fa fa-facebook-official"></i>
+				</a>
+
+			</a>div>
+
+			</div>
 		</center>
 	</div>
 @stop
